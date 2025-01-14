691682b531f1259682045e9c7235b25bc5b7a4a4
Minhyuk Kim <labcs@naver.com>
2025-01-14 14:05:08 +0900

    fix: workflow 코드 수정

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
-// ./build.sh 
-
 #!/bin/sh
+
+# 상위 디렉토리로 이동
 cd ../
-mkdir output
-cp -R ./CapstoneLocalLens/* ./output
+
+# output 디렉토리 생성
+mkdir -p output
+
+# CapstoneLocalLens의 파일을 output 디렉토리로 복사
+cp -R ./CapstoneLocalLens/* ./output/
+
+# output 디렉토리를 다시 CapstoneLocalLens로 복사하는 부분을 수정
 cp -R ./output ./CapstoneLocalLens/
